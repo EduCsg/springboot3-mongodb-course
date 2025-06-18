d42848040e5c8af064353069dfdd55d269cb30aa
Eduardo <eduardocasagrande71@gmail.com>
2025-06-17 21:08:35 -0300

    first commit

---
+++ src/test/java/com/du/mongoDbCourse/MongoDbCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.du.mongoDbCourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoDbCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
